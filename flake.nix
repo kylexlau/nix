dd0d30067f39c5b2c95732fa0588c048efca5845
kylexlau <kylexlau@gmail.com>
2024-01-20 00:20:31 +0800

    add nix config files

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "My Darwin system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    # home manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#pro
+    darwinConfigurations."pro" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        ./modules/nix-core.nix
+
+        # home manager
+        home-manager.darwinModules.home-manager {
+          users.users.kyle.home = "/Users/kyle";
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = inputs;
+          home-manager.users.kyle = import ./home;
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."pro".pkgs;
+  };
+}
---
+++ modules/nix-core.nix
@@ -0,0 +1,46 @@
+{ pkgs, lib, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    #git
+    #vim
+    #neofetch
+  ];
+
+  # Nix settings
+  nix.settings.substituters = [ "https://mirrors.ustc.edu.cn/nix-channels/store" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Use this instead of services.nix-daemon.enable if you
+  # don't wan't the daemon service to be managed for you.
+  # nix.useDaemon = true;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "x86_64-darwin";
+
+  nix.package = pkgs.nix;
+  programs.nix-index.enable = true;
+
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = lib.mkDefault true;
+    options = lib.mkDefault "--delete-older-than 7d";
+  };
+
+  # Disable auto-optimise-store because of this issue:
+  #  https://github.com/NixOS/nix/issues/7273
+  # "error: cannot link
+  # '/nix/store/.tmp-link-xxxxx-xxxxx' to
+  # '/nix/store/.links/xxxx': File exists"
+  nix.settings = {
+    auto-optimise-store = false;
+  };
+}
