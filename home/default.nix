d22c3129746e8958ce343db3c205dd10ecbc3d35
kylexlau <kylexlau@gmail.com>
2024-01-20 14:09:53 +0800

    add fish.nix

--- home/default.nix
+++ home/default.nix
@@ -1,34 +1,58 @@
-{ ... }:
+{ pkgs, ... }:
 
 let
-  # please replace xxx with your username
   username = "kyle";
 in
 {
   # import sub modules
   imports = [
-    ./core.nix
     ./git.nix
+    ./fish.nix
   ];
-
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
 
   home = {
     username = username;
     homeDirectory = "/Users/${username}";
-
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
     stateVersion = "23.05";
+
+    packages = with pkgs; [
+      # archives
+      zip
+      xz
+      unzip
+      p7zip
+  
+      # utils
+      ripgrep
+      jq
+      yq-go
+      fzf
+      aria2
+      socat
+      nmap
+    ];
   };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+  
+    # modern vim
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+    };
+
+    eza = {
+      enable = true;
+      enableAliases = true;
+      git = true;
+      icons = true;
+    };
+
+    skim = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+  };
 }
---
+++ home/fish.nix
@@ -0,0 +1,34 @@
+{ ... }:
+{
+ programs.fish = {
+  enable = true;
+  shellAliases = {
+    dy = "dig +noall +answer +additional $argv[1] @dns.toys";
+  };
+
+  shellAbbrs = {
+    tailf = "tail -f";
+    cat = "bat";
+    findport = "sudo lsof -iTCP -sTCP:LISTEN -n -P | grep";
+  };
+
+  functions = {
+    spot = "mdfind \"kMDItemDisplayName==\'\*\$argv\*\'cd\"";
+    genpass = ''
+    LC_ALL=C tr -dc "[:alnum:]" < /dev/urandom | head -c 20 | pbcopy
+    '';
+    proxy = ''
+    switch $argv
+    case "off"
+      set -e http_proxy
+      set -e https_proxy
+      echo 'http[s]_proxy erased.'
+    case "*"
+      set -gx http_proxy "http://127.0.0.1:7890"
+      set -gx https_proxy "http://127.0.0.1:7890"
+      echo 'https[s]_proxy set.'
+    end
+    '';
+  };
+ };
+}
