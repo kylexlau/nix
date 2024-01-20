103f47c34ca992bda160f5d837a5a0bf5ff8d4d3
kylexlau <kylexlau@gmail.com>
2024-01-20 08:11:39 +0800

    add some fish shell config

--- home/core.nix
+++ home/core.nix
@@ -9,18 +9,51 @@
     p7zip
 
     # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processer https://github.com/mikefarah/yq
-    fzf # A command-line fuzzy finder
-
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-
+    ripgrep
+    jq
+    yq-go
+    fzf
+    aria2
+    socat
+    nmap
   ];
 
   programs = {
+    fish = {
+      enable = true;
+
+      shellAliases = {
+        dy = "dig +noall +answer +additional $argv[1] @dns.toys";
+      };
+
+      shellAbbrs = {
+        tailf = "tail -f";
+        cat = "bat";
+        findport = "sudo lsof -iTCP -sTCP:LISTEN -n -P | grep";
+      };
+
+      functions = {
+        spot = "mdfind \"kMDItemDisplayName==\'\*\$argv\*\'cd\"";
+
+        genpass = ''
+        LC_ALL=C tr -dc "[:alnum:]" < /dev/urandom | head -c 20 | pbcopy
+        '';
+
+        proxy = ''
+        switch $argv
+        case "off"
+          set -e http_proxy
+          set -e https_proxy
+          echo 'http[s]_proxy erased.'
+        case "*"
+          set -gx http_proxy "http://127.0.0.1:7890"
+          set -gx https_proxy "http://127.0.0.1:7890"
+          echo 'https[s]_proxy set.'
+        end
+        '';
+      };
+    };
+
     # modern vim
     neovim = {
       enable = true;
