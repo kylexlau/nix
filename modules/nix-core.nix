0cc8d56bdfab67622258d23ca5255dec121a16dd
kylexlau <kylexlau@gmail.com>
2024-01-22 19:59:16 +0800

    add macos system settings

--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -1,6 +1,12 @@
 { pkgs, lib, ... }:
 
 {
+
+  imports = [
+  ./dotfiles
+  ./git.nix
+  ./fish.nix
+  ];
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
